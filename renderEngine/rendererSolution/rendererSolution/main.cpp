337913b18b69980dbe6fa309d473e05e633eba10
Dane Nilsson <dane.nilsson@gmail.com>
2023-07-01 19:24:27 +0200

    Worked on adding c++ libs

---
+++ renderEngine/rendererSolution/rendererSolution/main.cpp
@@ -0,0 +1,16 @@
+// #include <vulkan/vulkan.h>
+//#define GLFW_INCLUDE_VULKAN
+#include "dependencies\glfw-3.3.8\include\GLFW/glfw3.h"
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include "dependencies/glm-master/glm/vec4.hpp"
+#include "dependencies/glm-master/glm/mat4x4.hpp"
+// #include <glm/mat4x4.hpp>
+
+#include <iostream>
+
+int main()
+{
+	return 0; 
+}
