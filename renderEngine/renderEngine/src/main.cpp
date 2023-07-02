58b841535040429b26f436899c3caef3ec64d5d5
Christopher Nylen <Joffynylen@hotmail.com>
2023-07-02 23:37:58 +0200

    physical devices and queue families

--- renderEngine/renderEngine/src/main.cpp
+++ renderEngine/renderEngine/src/main.cpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include <cstdlib>
 #include <vector>
+#include <map>
+#include <optional>
 
 //The tutorial turned out to be out - of - date with the behavior of the latest SDK.
 //Try the new instructions under the "Testing" subchapter.
@@ -28,6 +30,16 @@ void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT
     }
 }
 
+struct QueueFamilyIndices 
+{
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() 
+    {
+        return graphicsFamily.has_value();
+    }
+};
+
 class HelloTriangleApplication {
 public:
     void run() 
@@ -44,10 +56,105 @@ private:
         createInstance();
         setupDebugMessenger();
         pickPhysicalDevice();
+
+    }
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) 
+    {
+        QueueFamilyIndices indices;
+
+       
+        uint32_t queueFamilyCount = 0;
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+        std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+        vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+        int i = 0;
+        for (const auto& queueFamily : queueFamilies) 
+        {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+            if (indices.isComplete()) 
+            {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
     }
     void pickPhysicalDevice() 
     {
+        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+        if (deviceCount == 0) 
+        {
+            throw std::runtime_error("failed to find GPUs with Vulkan support!");
+        }
 
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+        for (const auto& device : devices) 
+        {
+            if (isDeviceSuitable(device)) 
+            {
+                physicalDevice = device;
+                break;
+            }
+        }
+
+        // Use an ordered map to automatically sort candidates by increasing score
+        //std::multimap<int, VkPhysicalDevice> candidates;
+
+        //for (const auto& device : devices) {
+        //    int score = rateDeviceSuitability(device);
+        //    candidates.insert(std::make_pair(score, device));
+        //}
+
+        //// Check if the best candidate is suitable at all
+        //if (candidates.rbegin()->first > 0) {
+        //    physicalDevice = candidates.rbegin()->second;
+        //}
+        //else {
+        //    throw std::runtime_error("failed to find a suitable GPU!");
+        //}
+
+        //if (physicalDevice == VK_NULL_HANDLE) 
+        //{
+        //    throw std::runtime_error("failed to find a suitable GPU!");
+        //}
+    }
+    //int rateDeviceSuitability(VkPhysicalDevice device) 
+    //{
+    //    int score = 0;
+
+    //    // Discrete GPUs have a significant performance advantage
+    //    if (deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) {
+    //        score += 1000;
+    //    }
+
+    //    // Maximum possible size of textures affects graphics quality
+    //    score += deviceProperties.limits.maxImageDimension2D;
+
+    //    // Application can't function without geometry shaders
+    //    if (!deviceFeatures.geometryShader) {
+    //        return 0;
+    //    }
+
+    //    return score;
+    //}
+
+    bool isDeviceSuitable(VkPhysicalDevice device) 
+    {
+        QueueFamilyIndices indices = findQueueFamilies(device);
+
+
+
+        return indices.isComplete();
     }
     void setupDebugMessenger() 
     {
