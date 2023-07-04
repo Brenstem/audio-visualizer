85122cc75df8ab6ca00821d68b633173f236753b
Christopher Nylen <Joffynylen@hotmail.com>
2023-07-04 23:37:21 +0200

    added lua to GUI and some code to vulkan

--- renderEngine/renderEngine/src/main.cpp
+++ renderEngine/renderEngine/src/main.cpp
@@ -12,7 +12,10 @@
 //Try the new instructions under the "Testing" subchapter.
 //-Validation layers
 
-VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger) 
+VkResult CreateDebugUtilsMessengerEXT(VkInstance instance,
+    const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+    const VkAllocationCallbacks* pAllocator,
+    VkDebugUtilsMessengerEXT* pDebugMessenger) 
 {
     auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
     if (func != nullptr) {
@@ -56,7 +59,44 @@ private:
         createInstance();
         setupDebugMessenger();
         pickPhysicalDevice();
+        createLogicalDevice();
+    }
+    void createLogicalDevice()
+    {
+        QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
 
+        VkDeviceQueueCreateInfo queueCreateInfo{};
+        queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+        queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+        queueCreateInfo.queueCount = 1;
+
+        float queuePriority = 1.0f;
+        queueCreateInfo.pQueuePriorities = &queuePriority;
+
+        VkPhysicalDeviceFeatures deviceFeatures{};
+
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+        createInfo.pQueueCreateInfos = &queueCreateInfo;
+        createInfo.queueCreateInfoCount = 1;
+
+        createInfo.pEnabledFeatures = &deviceFeatures;
+
+        createInfo.enabledExtensionCount = 0;
+
+        if (enableValidationLayers) 
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else 
+        {
+            createInfo.enabledLayerCount = 0;
+        }
+        if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create logical device!");
+        }
+        vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
     }
     QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device) 
     {
@@ -87,7 +127,7 @@ private:
     }
     void pickPhysicalDevice() 
     {
-        VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
         uint32_t deviceCount = 0;
         vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
         if (deviceCount == 0) 
@@ -98,11 +138,11 @@ private:
         std::vector<VkPhysicalDevice> devices(deviceCount);
         vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
 
-        for (const auto& device : devices) 
+        for (const auto& physDevice : devices) 
         {
-            if (isDeviceSuitable(device)) 
+            if (isDeviceSuitable(physDevice))
             {
-                physicalDevice = device;
+                physicalDevice = physDevice;
                 break;
             }
         }
@@ -274,6 +314,7 @@ private:
 
     void cleanup() 
     {
+        vkDestroyDevice(device, nullptr);
         if (enableValidationLayers) 
         {
            DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
@@ -335,7 +376,11 @@ private:
 #endif
 
     VkInstance instance;
+    VkDevice device;
+    VkQueue graphicsQueue;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
     VkDebugUtilsMessengerEXT debugMessenger;
+
 };
 
 int main() {
